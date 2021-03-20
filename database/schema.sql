5f7ee4394075b413f9002e15fe7dc745eb29af2a
RahulJung <rahuljungchauhan@gmail.com>
2021-03-19 21:36:58 -0500

    - Create a database

---
+++ database/schema.sql
@@ -0,0 +1,17 @@
+DROP DATABASE IF EXISTS fuelpredictor;
+
+CREATE DATABASE fuelpredictor;
+
+USE fuelpredictor;
+
+CREATE TABLE userLogin (
+  id varchar(100) NOT NULL,
+  username varchar(50) DEFAULT NULL,
+  password varchar(1000) DEFAULT NULL
+);
+
+CREATE TABLE userPassword (
+  id varchar(100) NOT NULL,
+  username varchar(50) DEFAULT NULL,
+  password varchar(1000) DEFAULT NULL
+);
