eb6c56424c547d8b7bea13dbf5ebc8d9e5d95575
RahulJung <rahuljungchauhan@gmail.com>
2021-03-19 22:14:36 -0500

    - Create backend routes to register users - Create routes for user login

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const path = require("path");
 const axios = require("axios");
+const cors = require("cors");
+const { getUserInfo, registerUser } = require("../database/db");
 
 const port = 8080;
 const app = express();
@@ -12,6 +14,29 @@ app.use(express.json());
 // Example of a get request
 app.get("/", (req, res) => {
   res.send("Hello from the server!!!");
+});
+
+app.post("/login", (req, res) => {
+  getUserInfo(req.body.userName, req.body.password, (err, data) => {
+    if (err) {
+      console.log("Error getting data from server");
+      res.sendStatus(500);
+    } else {
+      res.send(data);
+    }
+  });
+});
+
+app.post("http://localhost:8080/register", (req, res) => {
+  console.log("bodyyyyyyyyyyyyyyyyyy", req.body);
+  registerUser(req.body.userName, req.body.password, (err, data) => {
+    if (err) {
+      console.log("Error posting data from server");
+      res.sendStatus(500);
+    } else {
+      res.sendStatus(200);
+    }
+  });
 });
 
 // Listen to port 8080
